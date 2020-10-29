f17e1778b4ae630fbb37d8ddf0f9f431e7b382de
paf106 <pabloavila106@gmail.com>
2020-10-29 13:24:41 +0100

    Arkanoid empezado

---
+++ Arkanoid/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Arkanoid
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picPelota = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picPelota)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // picPelota
+            // 
+            this.picPelota.Image = global::Arkanoid.Properties.Resources.baloncesto__1_;
+            this.picPelota.Location = new System.Drawing.Point(345, 175);
+            this.picPelota.Name = "picPelota";
+            this.picPelota.Size = new System.Drawing.Size(64, 64);
+            this.picPelota.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picPelota.TabIndex = 0;
+            this.picPelota.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picPelota);
+            this.Name = "FrmPrincipal";
+            this.Text = "Juego";
+            ((System.ComponentModel.ISupportInitialize)(this.picPelota)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPelota;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Arkanoid/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arkanoid
+{
+    public partial class FrmPrincipal : Form
+    {
+        private int direccion = 4;
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Cambia la dirección de la pelota en funcion del número que le pasemos por parámetro
+        /// </summary>
+        /// <param name="dir"> Numero de la direccion</param>
+        public void changeDirection(int dir)
+        {
+            switch (dir)
+            {
+                case 1:
+                    picPelota.Left = picPelota.Left - 1;
+                    picPelota.Top = picPelota.Top - 1;
+
+                    // Choque pared arriba
+                    if (picPelota.Top == 0)
+                    {
+                        direccion=
+                    }
+                    break;
+                case 2:
+                    // Para mover la pelota
+                    picPelota.Left = picPelota.Left + 1;
+                    picPelota.Top = picPelota.Top - 1;
+                    break;
+                // choque pared derecha
+                // si pelota.left
+                case 3:
+                    // Para mover la pelota
+                    picPelota.Left = picPelota.Left + 1;
+                    picPelota.Top = picPelota.Top + 1;
+                    break;
+                // choque pared derecha
+                // si pelota.left
+                case 4:
+                    // Para mover la pelota
+                    picPelota.Left = picPelota.Left - 1;
+                    picPelota.Top = picPelota.Top + 1;
+                    break;
+                    // choque pared derecha
+                    // si pelota.left
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            changeDirection(direccion);
+        }
+    }
+}
