c37c3c20bf6432dc09cfaefba5c3bd647d24a57f
paf106 <pabloavila106@gmail.com>
2020-11-26 10:57:41 +0100

    Poliformismo iniciada

---
+++ Polimorfismo/IElementoMultimedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    interface IElementoMultimedia
+    {
+        Boolean buscar(String titulo);
+        string 
+    }
+}
